dd76727a3169eb28adcc74e9452cb80a57bc1682
ayomide onibokun <ayo6706@gmail.com>
2019-10-18 11:59:45 -0700

    cbtexam

---
+++ admin/inc/footer.php
@@ -0,0 +1,7 @@
+ </section>
+<section class="footeroption">
+		<h3><?php echo "ADMIN PANEL Designed By: OMIDIRAN INIOLUWA DAVID - U/15/CS/208"; ?></h3>
+	</section>
+</div>
+</body>
+</html>
---
+++ exam.php
@@ -0,0 +1,22 @@
+<?php include 'inc/header.php'; ?>
+
+<?php
+Session::checkSession();
+?>
+
+<div class="main">
+<h1>Welcome to Online Exam - Start Here</h1>
+	<div class="segment" style="margin-right:30px;">
+		<img src="img/online_exam.png"/>
+	</div>
+	<div class="segment">
+	<h2>Online Exam</h2>
+        <p style="text-align: justify; display: block; font-size: 16px; color: #444444">Online examination is an app for conducting tests online to measure the knowledge of the applicants on a given topic.
+           This is a form of test which would help you prove your general intellect, and at the same time your level of IQ. Aptitude test is a plus along with other talents and qualifications. Once you have proved your aptitude in the field you can indeed yield better result in career. We have provided a scope to test your aptitude in order to have a better idea about your general skill in the field.</p>
+	<ul style="padding-top: 20px; padding-left: 50px;">
+		<li><a href="start_test.php">Start Here...</a></li>
+	</ul>
+	</div>
+	
+  </div>
+<?php include 'inc/footer.php'; ?>
