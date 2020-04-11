32abdd2ef05184d1fcc8bf716e5c2fe324b23f9c
ayomide onibokun <35877863+ayo6706@users.noreply.github.com>
2020-04-11 21:51:31 +0100

    Update footer.php

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -1,6 +1,6 @@
  </section>
 <section class="footeroption">
-		<h3><?php echo "ADMIN PANEL Designed By: OMIDIRAN INIOLUWA DAVID - U/15/CS/208"; ?></h3>
+		<h3><?php echo "ADMIN PANEL Designed By: Vadton Tech"; ?></h3>
 	</section>
 </div>
 </body>
